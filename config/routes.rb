251ad359a8ffb714ba313a3d51ffb989f659aac3
hugokl <hugokl@outlook.com>
2016-02-05 03:27:43 +0000

    Mudei alguns arquivos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  root to: "products#index"
   resources :products, only: [:create, :new, :index] do
     resources :factories, only: [:new, :create]
     resources :sales, only: [:new, :create]
